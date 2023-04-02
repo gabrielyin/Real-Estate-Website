737122ad6dcf926b645881e9ccc6ef7119fa6c16
gabrielyin <2021gabrielyin@gmail.com>
2023-04-02 16:53:21 -0300

    property dropdown

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -30,6 +30,19 @@ const HouseContextProvider = ({ children }) => {
     setCountries(uniqueCountries);
   },[])
 
+    //return all properties
+    useEffect(() => {
+      const allProperties = houses.map((house) => {
+        return house.type
+      });
+  
+      //remove duplicates
+      const uniqueProperties = ['Location (any)', ...new Set(allProperties)]
+          
+      // set properties state
+      setProperties(uniqueProperties);
+    },[])
+
   return (
     <HouseContext.Provider value={{
       country,
